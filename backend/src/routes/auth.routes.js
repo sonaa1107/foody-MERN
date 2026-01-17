470f68e2edcfe6effd80033893a286a48963498e
sonaa1107 <sonaagrawal1107@gmail.com>
2026-01-18 00:44:16 +0530

    complete user routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,7 +1,10 @@
 const express=require('express');
-const { userRegister } = require('../db/controller/auth.controller');
-const router=express.Router
+const { userRegister, userLogin, userLogout } = require('../controller/auth.controller');
+const router=express.Router();
 
 router.post('/user/register',userRegister)
+router.post('/user/login',userLogin)
+router.get('/user/logout',userLogout)
+
 
 module.exports=router;
