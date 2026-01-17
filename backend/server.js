56d39021caf1d6b100e39caec1fa573f129ce472
sonaa1107 <sonaagrawal1107@gmail.com>
2026-01-18 00:46:25 +0530

    env intro

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 // start server
+require('dotenv').config();
 const app=require('./src/app');
 const connectDB = require('./src/db/db');
 
-connectDB;
+connectDB();
 
 app.listen(3000,()=>{
     console.log('server started on port 3000');
