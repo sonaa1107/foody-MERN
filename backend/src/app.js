99e1720691686453a91ddda11a3e31245fd099ec
sonaa1107 <sonaagrawal1107@gmail.com>
2026-01-19 11:27:23 +0530

    add new packages

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,13 @@
 const express=require('express');
 const cookieParser = require('cookie-parser');
 const userAndFoodPartnerRouter=require('./routes/auth.routes');
+const foodRouter=require('./routes/food.routes')
 
 const app=express();
 app.use(cookieParser()); 
 app.use(express.json());
 app.use('/api/auth',userAndFoodPartnerRouter);
+app.use('/api/food',foodRouter);
 
 app.get("/",(req,res)=>{
     res.send("Hello world");
